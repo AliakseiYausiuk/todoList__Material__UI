4d648375071ab7f1945ae6800a3b66c94eed9eb3
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2020-07-09 20:41:47 +0300

    Updates

---
+++ precache-manifest.25ae777b81d1df960e0444274d1c12be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2d2239c8b306d63d0174d83369a357c",
+    "url": "/todoList__Material__UI/index.html"
+  },
+  {
+    "revision": "9af4a12df54771055bfd",
+    "url": "/todoList__Material__UI/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "72030941449ebbefcacd",
+    "url": "/todoList__Material__UI/static/js/2.8d817e7e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todoList__Material__UI/static/js/2.8d817e7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9af4a12df54771055bfd",
+    "url": "/todoList__Material__UI/static/js/main.57f36e37.chunk.js"
+  },
+  {
+    "revision": "9f025e6571bcf337ea47",
+    "url": "/todoList__Material__UI/static/js/runtime-main.4790b6be.js"
+  }
+]);
